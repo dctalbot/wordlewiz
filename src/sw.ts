7a0ff643559d7e3a644c3a1835cac71914218778
dctalbot <david.talbot@target.com>
2022-02-11 18:57:15 -0600

    add letter map

--- src/sw.ts
+++ src/sw.ts
@@ -1,16 +1,55 @@
 import words from "./words.json";
 
+// setInterval(() => {
+//   const a = Math.floor(Math.random() * 100);
+//   const options = words.slice(a, a + 5);
+//   chrome.storage.local.set({ options });
+//   console.log(options);
+// }, 5000);
+
+// character means correct, set means exclude
+const init: (string | Set<string>)[] = new Array(5).fill(new Set());
+
 setInterval(() => {
-  const a = Math.floor(Math.random() * 100);
-  const options = words.slice(a, a + 5);
-  chrome.storage.local.set({ options });
-  console.log(options);
+  chrome.storage.local
+    .get("wordleState")
+    .then((val) => console.log(update(init, val.wordleState)));
 }, 5000);
 
-let wordleState = {};
-
 chrome.storage.onChanged.addListener((store) => {
-  wordleState = store.wordleState?.newValue ?? wordleState;
+  const wordleState: WorldleState = store.wordleState?.newValue;
 
-  chrome.runtime.sendMessage({ options: words });
+  // chrome.runtime.sendMessage({ options: words });
+
+  const letters = update(init, wordleState);
+  console.log(letters);
 });
+
+export function update(letters: typeof init, w: WorldleState) {
+  if (w.rowIndex === 0) return letters;
+
+  letters.forEach((val, i) => {
+    if (typeof i === "string") return;
+
+    let eIndex = 5;
+
+    while (eIndex >= 0) {
+      const row = w.evaluations[eIndex];
+
+      if (row === null) {
+        eIndex--;
+        continue;
+      }
+
+      if (row[i] === "correct") {
+        letters[i] = w.boardState[eIndex][i];
+        break;
+      }
+
+      letters[i] = (letters[i] as Set<string>).add(w.boardState[eIndex][i]);
+      eIndex--;
+    }
+  });
+
+  return letters;
+}
